8d775bdb081cc240e7358e40c6e2e6c399d6e1ba
Rodion Rybko <rybkorodion201@mail.ru>
2025-03-20 00:28:58 +0700

    create hook useRequest

--- tsu-app/src/components/pages/newRequestPage/newRequestCardForm/NewRequestCardForm.tsx
+++ tsu-app/src/components/pages/newRequestPage/newRequestCardForm/NewRequestCardForm.tsx
@@ -2,84 +2,15 @@ import "./newRequestCardForm.css"
 
 import Input from "../../../ui/input/Input";
 import Button from "../../../ui/button/Button";
-import { ROUTES } from "../../../../utils/routes";
-import { ERROR_MESSAGES } from "../../../../utils/errorMessages";
 import FixedPhotoCard from "../../../ui/fixedPhotoCard/fixedPhotoCard";
 
-import { createRequest } from "../../../../utils/api/createRequest";
+import { ERROR_MESSAGES } from "../../../../utils/errorMessages";
 
-import { RequestData } from "../../../../@types/api";
-
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useRequest } from "../hooks/useRequest";
 
 const NewRequestForm = () => { 
 
-    const navigate = useNavigate();
-
-    const [requestData, setRequestData] = useState<RequestData>({description: "", absenceDateFrom: null, absenceDateTo: null, photos: []});
-    const [errorStatusCode, setErrorStatusCode] = useState<number>(0);
-    const [errorFlag, setErrorFlag] = useState<boolean>(false);
-
-    const handleChange = (field: string, value: string | File[]) => {
-        setRequestData((prev) => (
-            {...prev, [field]: Array.isArray(value) ? [...prev.photos, ...value] : value}
-        ))
-    }
-
-    const clearFiles = () => {
-        setRequestData((prev) => (
-            {...prev, ["photos"]: []}
-        ))
-    };
-
-    const removeFile = (id: number) => {
-        setRequestData((prev) => (
-            {...prev, ["photos"]: prev['photos'].filter((item, index) => index !== id)}
-        ))
-    }
-
-    
-
-    const handleClick = async () => {
-        // Пока так берем токен, потом будем брать из global state
-        const token = localStorage.getItem("token");
-
-        if (requestData.description.length === 0){
-            setErrorFlag(true)
-            setErrorStatusCode(7);
-        }
-        else if (Date.now() <= requestData.absenceDateFrom){
-            setErrorFlag(true)
-            setErrorStatusCode(9)
-        }
-        else if ((requestData.absenceDateFrom) > (requestData.absenceDateTo)){
-            setErrorFlag(true)
-            setErrorStatusCode(10)
-        }
-        else{
-            setErrorFlag(false)
-            setErrorStatusCode(0);
-
-            try{
-                if (token){
-                    const formData = new FormData();
-                    formData.append("absenceDateFrom", requestData.absenceDateFrom);
-                    formData.append("absenceDateTo", requestData.absenceDateTo);
-                    formData.append("description", requestData.description);
-                    requestData.photos.forEach((file) => {
-                        formData.append("files", file);
-                    });
-                    await createRequest(token, formData);
-                    navigate(ROUTES.MAINPAGE);
-                }
-            }
-            catch{
-                // Сейчас проведем запрос даты
-                console.log("error");
-            }
-        }
-    }
+    const { requestData, errorStatusCode, errorFlag, handleChange, handleClick, clearFiles, removeFile } = useRequest();
 
     return (
         <>
